d34e050d412bfab16c848c08fba472b260112381
Felo Vilches <felovilches@gmail.com>
2017-08-31 19:22:11 -0300

    alternar entre geo y manual

--- www/js/controllers/agendamiento/tomar_agenda.js
+++ www/js/controllers/agendamiento/tomar_agenda.js
@@ -3,6 +3,25 @@ angular.module('movistar')
 .controller('AgendamientoTomarAgendaController', function($scope, geo, ionicDatePicker, sucursal, motivo) {
 
   $scope.motivosAtencion = [];
+  $scope.usandoGeolocalizacion = true;
+
+
+  // Cuando el usuario abre la vista de agenda por primera vez, y la geolocalizacion
+  // ha fallado, la idea es redireccionar al usuario a la vista de busqueda manual.
+  // Sin embargo, si el usuario decide el mismo ir a la vista de busqueda por geolocalizacion,
+  // y vuelve a fallar, la idea es que ya no se redireccione. Para eso sirve esta bandera.
+  var usuarioEligioMetodoBusqueda = false;
+
+
+  $scope.usarGeo = function(){
+    $scope.usandoGeolocalizacion = true;
+    usuarioEligioMetodoBusqueda = true;
+  }
+
+  $scope.usarManual = function(){
+    $scope.usandoGeolocalizacion = false;
+    usuarioEligioMetodoBusqueda = true;
+  }
 
   $scope.sucursalElegida = {
     sucursal: null
@@ -48,6 +67,11 @@ angular.module('movistar')
 
       if(!pos){
         $scope.geoEstado = "FALLO";
+
+        if(!usuarioEligioMetodoBusqueda){
+          $scope.usarManual();
+        }
+
         return;
       }
 
@@ -60,6 +84,11 @@ angular.module('movistar')
         } else {
           $scope.geoEstado = "NO_HAY_SUCURSALES_CERCANAS";
           $scope.sucursalesCercanas = [];
+
+          if(!usuarioEligioMetodoBusqueda){
+            $scope.usarManual();
+          }
+
         }
       });
 
